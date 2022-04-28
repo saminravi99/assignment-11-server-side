295c2198effe82841fa6169c04d7a76a31cf8b81
Samin Israr Ravi <saminisrar1@gmail.com>
2022-04-28 23:18:07 +0600

    server running

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
@@ -22,6 +23,11 @@ const run = async () => {
     console.log("Connected to MongoDB");
     const db = client.db("booksDb");
     const booksCollection = db.collection("booksCollection");
+
+    // API to Run Server
+    app.get("/", async (req, res) => {
+        res.send("Warehouse Server Running");
+    });
 
     // API to get List of All Books
 
