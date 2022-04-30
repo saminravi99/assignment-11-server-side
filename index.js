2539cafe34462e5395fbc326e707eb669dfd3aae
Samin Israr Ravi <saminisrar1@gmail.com>
2022-04-30 19:24:29 +0600

    server updated

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
+// app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
