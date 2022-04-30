4eb90af4d5bd28f31dd5368cfc33372cb21e23de
Samin Israr Ravi <saminisrar1@gmail.com>
2022-04-30 22:02:37 +0600

    server updated

--- index.js
+++ index.js
@@ -7,14 +7,9 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions)); // Use this after the variable declaration
 
+app.use(cors()); 
 // app.use(cors());
 app.use(express.json());
 
