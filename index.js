bc0f8e28d5cffd9872e20ad80b82b52936b61d54
Samin Israr Ravi <saminisrar1@gmail.com>
2022-05-01 11:55:12 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ const run = async () => {
       const decodedEmail = req.decoded.email;
       const email = req.headers.email;
       const queryEmail = req.query.email;
-      if (queryEmail === decodedEmail) {
+      if (email === decodedEmail) {
         const query = { email: queryEmail };
         // console.log(query);
         const user = await userAddItemCollection.find(query).toArray();
